2bc54c64ff66881a11f48071dfac3094e951f84d
Ryan Nitz <ryan.nitz@coinbase.com>
2025-05-13 12:22:30 -0400

    Update to v0.2.0

--- Formula/prime-cli.rb
+++ Formula/prime-cli.rb
@@ -1,9 +1,10 @@
 class PrimeCli < Formula
   desc "Coinbase Prime command-line interface (CLI) "
   homepage "https://github.com/coinbase-samples/prime-cli"
-  url "https://github.com/coinbase-samples/prime-cli/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "8fd761c37266ad5dcdd738510209c883e5784fbea0efbe12f392c2201367a60b"
+  url "https://github.com/coinbase-samples/prime-cli/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "dffd489e6e3d036e521f3decf6bfda4cce6f4825a7b16ea3d66c3d50573c8085"
   license "Apache-2.0"
+  version "0.2.0"
 
   depends_on "go" => :build
 
